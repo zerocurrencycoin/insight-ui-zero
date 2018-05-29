c8f661f716838ea52e81680d81e2ce6bd03cbb5a
ProphetAlgorithms <38349760+ProphetAlgorithms@users.noreply.github.com>
2018-05-29 19:41:43 +0200

    changes for the Zero node
    enable insight-ui to work with the Zero node and to create the insight-ui-zero package

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var testnet = false;
-var netSymbol = testnet ? 'TAZ' : 'ZEC';
+var netSymbol = testnet ? 'TAZ' : 'ZER';
 
 var defaultLanguage = localStorage.getItem('insight-language') || 'en';
 var defaultCurrency = localStorage.getItem('insight-currency') || netSymbol;
